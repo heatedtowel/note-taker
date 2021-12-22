42593b246f413fc0da1ab82dd638e32f777d0ea9
Jesse Monks <monksjesse@gmail.com>
2021-12-21 23:18:16 -0500

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const port = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
 const fs = require('fs');
 const util = require('util');
